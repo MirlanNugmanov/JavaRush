8a05260db1d0787d6d421a55a296f3a6191b2801
nugmmir <mirlan.nugmanov@deutsche-boerse.com>
2024-03-15 13:30:49 +0100

    first

--- src/main/java/FirstFile.java
+++ src/main/java/FirstFile.java
@@ -1,2 +1,10 @@
 public class FirstFile {
+    public static void main(String[] args) {
+        int six = 6;
+        six++;
+        six++;
+        six++;
+
+        System.out.println(six);
+    }
 }
